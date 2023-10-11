845d8922fb9a698c7f09b56a3e6d0c9e5f485cf4
Dan Engel <dengel29@gmail.com>
2023-10-11 22:29:38 +0800

    using updated useCurrentUser hook return value with error if needed

--- app/src/LoggedIn.tsx
+++ app/src/LoggedIn.tsx
@@ -1,19 +1,31 @@
 import { Link } from "react-router-dom";
 import { PageContainer } from "./PageContainer";
 import { useCurrentUser } from "./hooks/useCurrentUser";
+import { useState, useEffect } from "react";
 
 export const LoggedInPage = (): JSX.Element => {
-  const { currentUser, isLoading } = useCurrentUser();
+  const { currentUser, loading, error } = useCurrentUser();
+  const [loginStatus, setLoginStatus] = useState<string>();
 
-  const loginStatus = isLoading ? (
-    <h1>Making sure you're logged in...</h1>
-  ) : (
-    <h1>Hey, {currentUser?.email} you're logged in, that's neat</h1>
-  );
+  useEffect(() => {
+    const determineLoginStatus = () => {
+      if (loading) {
+        setLoginStatus("Making sure you're logged in..");
+      } else if (currentUser && !loading) {
+        setLoginStatus(
+          `Hey, ${currentUser.email} you're logged in, that's neat`
+        );
+      } else if (error && !loading) {
+        setLoginStatus(error);
+      }
+    };
+
+    determineLoginStatus();
+  }, [error, loading, currentUser]);
 
   return (
     <PageContainer title={"Success!"}>
-      {loginStatus}
+      <h1>{loginStatus}</h1>
       <Link to="/home">Go home and get started</Link>
       {/* <button onClick={() => findMe()}>See if you're logged in</button> */}
     </PageContainer>
