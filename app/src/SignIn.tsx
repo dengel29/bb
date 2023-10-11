69194b94920f7e2aae97de93e5278e4aa4852649
Dan Engel <dengel29@gmail.com>
2023-10-11 22:30:20 +0800

    clearer messages for users after email is sent

--- app/src/SignIn.tsx
+++ app/src/SignIn.tsx
@@ -1,9 +1,14 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const SignIn = (): JSX.Element => {
   const email = useRef<HTMLInputElement>(null);
+  const [emailSent, setEmailSent] = useState<boolean | null>(null);
+  const [emailError, setEmailError] = useState<boolean | null>(null);
+  const [inputsDisabled, setInputsDisabled] = useState(false);
   const submitEmail = () => {
     console.log(email.current?.value);
+    setInputsDisabled(true);
+    setEmailError(false);
     fetch(`http://localhost:3000/auth/magiclogin`, {
       method: `POST`,
       body: JSON.stringify({
@@ -20,12 +25,16 @@ const SignIn = (): JSX.Element => {
       })
       .then((json) => {
         if (json.success) {
+          setEmailSent(true);
           // The request successfully completed and the email to the user with the
           // magic login link was sent!
           // You can now prompt the user to click on the link in their email
           // We recommend you display json.code in the UI (!) so the user can verify
           // that they're clicking on the link for their _current_ login attempt
-          document.body.innerText = json.code;
+          // document.body.innerText = json.code;
+        } else {
+          setEmailError(true);
+          setInputsDisabled(false);
         }
       });
   };
@@ -35,8 +44,22 @@ const SignIn = (): JSX.Element => {
         We'll send you a magic link to sign in with, please enter your email:
       </h1>
       <label htmlFor="email">Hey</label>
-      <input type="text" name="email" ref={email} />
-      <button onClick={() => submitEmail()}>Submit</button>
+      <input type="email" name="email" ref={email} disabled={inputsDisabled} />
+      <button onClick={() => submitEmail()} disabled={inputsDisabled}>
+        Submit
+      </button>
+      {emailSent && (
+        <h2>
+          Bingo, your email has been sent to {email.current?.value}, please go
+          to your inbox and click the link
+        </h2>
+      )}
+      {emailError && (
+        <h2>
+          It seems the email you supplied had an issue, can you double-check to
+          make sure it's correct?
+        </h2>
+      )}
     </div>
   );
 };
