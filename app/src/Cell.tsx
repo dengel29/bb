2ba0f356bd9e640f20019deff1af8e7921b572b7
Dan Engel <dengel29@gmail.com>
2023-11-14 15:56:05 +0800

    cell now has local count state, triggers click handler when count is reached

--- app/src/Cell.tsx
+++ app/src/Cell.tsx
@@ -1,23 +1,50 @@
-// import { MouseEventHandler } from 'react';
-// import { socket } from './socket';
 import { Text } from "@radix-ui/themes";
+import { useState } from "react";
 
 export const BingoCell = ({
   text,
   cellId,
   handleClick,
-  // determineOwner,
+  countable,
+  countLimit,
   owner,
-  // score,
+  sharedStyle,
+}: {
+  text: string;
+  cellId: number;
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  countable: boolean;
+  countLimit?: number;
+  owner: string;
+  sharedStyle?: string;
 }): JSX.Element => {
+  const [count, setCount] = useState(0);
+  const handleCellClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (countable && count === countLimit) {
+      const newCount = count - 1;
+      setCount(newCount);
+      handleClick(event);
+    } else if (countable) {
+      const newCount = count + 1;
+      setCount(newCount);
+      if (newCount === countLimit) {
+        handleClick(event);
+      }
+    } else {
+      handleClick(event);
+    }
+  };
   return (
     <button
-      onClick={handleClick}
+      onClick={handleCellClick}
       data-id={cellId}
       key={cellId}
       style={{
-        height: "150px",
+        minHeight: "150px",
         width: "100%",
+        maxHeight: "200px",
+        maxWidth: "200px",
+        background: `${sharedStyle}`,
       }}
       className={`cell ${owner}`}
     >
@@ -31,6 +58,7 @@ export const BingoCell = ({
       >
         {text}
       </Text>
+      {countable && <Text className={`opaque bottom-right`}>{count}</Text>}
     </button>
   );
 };
